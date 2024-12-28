173cb0f8d068e6553671a4192963a5e23a300622
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2024-12-27 23:57:23 -0800

    Add train test split

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
 
@@ -24,27 +24,30 @@ class MatrixTransformer(nn.Module):
 
 
 def train(model, dataloader, criterion, optimizer, num_epochs=5):
-
     model.train()
-
     for epoch in range(num_epochs):
         epoch_loss = 0.0
-
         for images, _ in tqdm(dataloader):
-
             images = images.to(device)
-
             outputs = model(images)
-
             loss = criterion(outputs, images)
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
             epoch_loss += loss.item()
-
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss / len(dataloader):.4f}")
+
+
+def test(model, dataloader, criterion):
+    model.eval()
+    test_loss = 0.0
+    with torch.no_grad():
+        for images, _ in tqdm(dataloader):
+            images = images.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, images)
+            test_loss += loss.item()
+    print(f"Test Loss: {test_loss / len(dataloader):.4f}")
 
 
 transform = transforms.Compose([
@@ -52,8 +55,17 @@ transform = transforms.Compose([
     transforms.Normalize((0.5,), (0.5,))
 ])
 
-train_dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+full_train_dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+
+train_size = int(0.70 * len(full_train_dataset))
+val_size = len(full_train_dataset) - train_size
+train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size])
+
+test_dataset = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = MatrixTransformer().to(device)
@@ -61,3 +73,9 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 train(model, train_loader, criterion, optimizer, num_epochs=5)
+
+print("Evaluating on validation set:")
+test(model, val_loader, criterion)
+
+print("Evaluating on test set:")
+test(model, test_loader, criterion)
