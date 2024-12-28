31ab06a38c594811731c067a851a983c47269afb
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2024-12-27 23:55:08 -0800

    Add another smaller layer

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ class MatrixTransformer(nn.Module):
     def __init__(self):
         super(MatrixTransformer, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, padding=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = self.conv3(x)
+        x = F.relu(self.conv3(x))
+        x = self.conv4(x)
         return x
 
 
